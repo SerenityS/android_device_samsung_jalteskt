d892853c2a5a8b1b196e7d575a2cd1da0461bdf2
SerenityS <jins4218@naver.com>
2016-06-28 02:11:35 +0900

    audio: Add more audio config && Delete unused

--- jalteskt.mk
+++ jalteskt.mk
@@ -57,7 +57,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/tinyucm.conf:system/etc/tinyucm.conf \
-    $(LOCAL_PATH)/configs/audio/ymc_debug.sh:system/etc/ymc_debug.sh \
+    $(LOCAL_PATH)/configs/audio/default_gain.conf:system/etc/default_gain.conf \
     $(LOCAL_PATH)/configs/audio/ysound.xml:system/etc/ysound.xml
 
 PRODUCT_PACKAGES += \
