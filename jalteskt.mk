bafe817270185d7a47f648d033339f711138b637
SerenityS <jins4218@naver.com>
2016-07-02 14:55:53 +0900

    camera: Build camera library
    Kanged from CM

--- jalteskt.mk
+++ jalteskt.mk
@@ -83,6 +83,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     camera2.portability.force_api=1
 
+PRODUCT_PACKAGES += \
+    camera.universal5410
+
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
